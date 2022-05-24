037b425916d75e0d811464ccc3ba2ad445b380bc
vsp <github.com/vishnusureshperumbavoor>
2022-05-24 16:56:43 +0530

    react alice corousel successfull

--- src/Banner/Carousel.js
+++ src/Banner/Carousel.js
@@ -4,18 +4,31 @@ import axios from 'axios'
 import { CryptoState } from '../CryptoContext'
 import { TrendingCoins } from '../Config/Api'
 import { useState } from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles(()=>({
     Carousel:{
         height:"50%",
         display:"flex",
         alignItems:"center"
+    },
+    CarouselItem:{
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center",
+        cursor:"pointer",
+        textTransform:"uppercase",
+        color:"white"
     }
 }))
+export function numberWithCommas(x){
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g,",");
+}
 
 function Carousel() {
   const classes = useStyles();
-  const {currency} = CryptoState();
+  const {currency,symbol} = CryptoState();
   const [trending,setTrending] = useState([])
   const fetchTrendingCoins=async()=>{
       const {data} = await axios.get(TrendingCoins(currency))
@@ -25,8 +38,37 @@ function Carousel() {
   useEffect(()=>{
       fetchTrendingCoins();
   },[currency])
+
+  const items=trending.map((coin)=>{
+      let profit = coin.price_change_percentage_24h >= 0
+      return(
+          <Link className={classes.CarouselItem} to={`/coins/${coin.id}`} >
+              <img src={coin?.image} alt={coin.name} height="80" style={{marginBottom:10}} />
+              <span>{coin?.symbol} &nbsp; 
+              <span style={{color:profit>0 ? "rgb(14,203,129)" : "red" ,
+               fontWeight:500}}
+              >{profit && '+'} {coin?.price_change_percentage_24h?.toFixed(2)}% </span> 
+              </span>
+
+              <span style={{fontSize:22,fontWeight:500}}>
+                  {symbol}{numberWithCommas(coin?.current_price.toFixed())}
+              </span>
+          </Link>
+      )
+  })
+
+  const responsive={
+      0:{
+          items:2
+      },
+      512:{
+          items:4
+      }
+  }
+
   return (
-    <div className={classes.Carousel}>Carousel</div>
+    <div className={classes.Carousel}> <AliceCarousel mouseTracking infinite autoPlayInterval={1000} animationDuration={1500} 
+    disableDotsControls responsive={responsive} autoPlay items={items} /> </div>
   )
 }
 
